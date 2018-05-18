5d89576d80d8fc749d4afa4c1db9440632a8132f
rlemchar <lemcharkiroxane@gmail.com>
2018-05-18 16:27:06 +0200

    testing html page

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ var app = express();
 
 app.get('/', function(req, res) {
     res.setHeader('Content-Type', 'text/plain');
-    res.send('Vous êtes à l\'accueil');
+    res.sendFile('views/index.html');
 });
 
 app.listen(8080);
