d5c2dab2cc7e17f6ff30a9ba69447814460c445d
helo-ch <hchevalier33@gmail.com>
2018-06-15 21:36:18 +0200

    test

--- public/scripts/scriptRecherche.js
+++ public/scripts/scriptRecherche.js
@@ -157,7 +157,7 @@ function results() {
     };
 
     // Accède au xml source dans une URL 
-    var path = "/recherche/bdd";
+    var path = "http://www.k-upload.fr/afficher-fichier-2018-06-15-1307d1d79test.xml.html";
     xmlhttp.open("GET", path, true);
     xmlhttp.send(null);
 }
